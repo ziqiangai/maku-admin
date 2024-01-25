e2d281724000a75f42dcb857b82ec333a47c8ef7
ziqiang <1694392889@qq.com>
2024-01-26 04:30:14 +0800

    feat: list component

--- src/views/ass/check/first.vue
+++ src/views/ass/check/first.vue
@@ -37,18 +37,21 @@
 			@selection-change="selectionChangeHandle"
 		>
 			<el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-			<el-table-column prop="username" label="用户名" header-align="center" align="center"></el-table-column>
-			<el-table-column prop="realName" label="姓名" header-align="center" align="center"></el-table-column>
-			<fast-table-column prop="gender" label="性别" dict-type="user_gender"></fast-table-column>
-			<el-table-column prop="mobile" label="手机号" header-align="center" align="center"></el-table-column>
-			<el-table-column prop="email" label="邮箱" header-align="center" align="center"></el-table-column>
-			<el-table-column prop="orgName" label="所属机构" header-align="center" align="center"></el-table-column>
-			<fast-table-column prop="status" label="状态" dict-type="user_status"></fast-table-column>
-			<el-table-column prop="createTime" label="创建时间" header-align="center" align="center" width="170"></el-table-column>
+			<el-table-column prop="itemName" label="采购物品名称" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="quantity" label="数量" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="unitPrice" label="单价" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="totalPrice" label="总价" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="note" label="备注" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="manufacturer" label="厂家" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="purchaseReason" label="采购理由" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="category" label="类目" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="status" label="采购状态" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="approvalStatus" label="审批状态" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="approvalComment" label="审批备注" header-align="center" align="center"></el-table-column>
 			<el-table-column label="操作" fixed="right" header-align="center" align="center" width="150">
 				<template #default="scope">
-					<el-button v-auth="'sys:user:update'" type="primary" link @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
-					<el-button v-auth="'sys:user:delete'" type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
+					<el-button type="primary" link @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+					<el-button type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
 				</template>
 			</el-table-column>
 		</el-table>
--- src/views/ass/check/list.vue
+++ src/views/ass/check/list.vue
@@ -1,31 +1,14 @@
-first.vue<template>
+<template>
 	<el-card>
 		<el-form :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
 			<el-form-item>
-				<el-input v-model="state.queryForm.username" placeholder="用户名" clearable></el-input>
+				<el-input v-model="state.queryForm.name" placeholder="名称" clearable></el-input>
 			</el-form-item>
 			<el-form-item>
-				<el-input v-model="state.queryForm.mobile" placeholder="手机号" clearable></el-input>
-			</el-form-item>
-			<el-form-item>
-				<fast-select v-model="state.queryForm.gender" dict-type="user_gender" clearable placeholder="性别"></fast-select>
+				<el-input v-model="state.queryForm.reason" placeholder="理由" clearable></el-input>
 			</el-form-item>
 			<el-form-item>
 				<el-button @click="getDataList()">查询</el-button>
-			</el-form-item>
-			<el-form-item>
-				<el-button v-auth="'sys:user:save'" type="primary" @click="addOrUpdateHandle()">新增</el-button>
-			</el-form-item>
-			<el-form-item>
-				<el-button v-auth="'sys:user:delete'" type="danger" @click="deleteBatchHandle()">删除</el-button>
-			</el-form-item>
-			<el-form-item v-auth="'sys:user:import'">
-				<el-upload :action="uploadUserExcelUrl" :before-upload="beforeUpload" :on-success="handleSuccess" :show-file-list="false">
-					<el-button type="info">导入</el-button>
-				</el-upload>
-			</el-form-item>
-			<el-form-item>
-				<el-button v-auth="'sys:user:export'" type="success" @click="downloadExcel()">导出</el-button>
 			</el-form-item>
 		</el-form>
 		<el-table
@@ -37,21 +20,25 @@ first.vue<template>
 			@selection-change="selectionChangeHandle"
 		>
 			<el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-			<el-table-column prop="username" label="用户名" header-align="center" align="center"></el-table-column>
-			<el-table-column prop="realName" label="姓名" header-align="center" align="center"></el-table-column>
-			<fast-table-column prop="gender" label="性别" dict-type="user_gender"></fast-table-column>
-			<el-table-column prop="mobile" label="手机号" header-align="center" align="center"></el-table-column>
-			<el-table-column prop="email" label="邮箱" header-align="center" align="center"></el-table-column>
-			<el-table-column prop="orgName" label="所属机构" header-align="center" align="center"></el-table-column>
-			<fast-table-column prop="status" label="状态" dict-type="user_status"></fast-table-column>
-			<el-table-column prop="createTime" label="创建时间" header-align="center" align="center" width="170"></el-table-column>
+			<el-table-column prop="itemName" label="采购物品名称" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="quantity" label="数量" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="unitPrice" label="单价" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="totalPrice" label="总价" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="note" label="备注" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="manufacturer" label="厂家" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="purchaseReason" label="采购理由" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="category" label="类目" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="status" label="采购状态" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="approvalStatus" label="审批状态" header-align="center" align="center"></el-table-column>
+			<el-table-column prop="approvalComment" label="审批备注" header-align="center" align="center"></el-table-column>
 			<el-table-column label="操作" fixed="right" header-align="center" align="center" width="150">
 				<template #default="scope">
-					<el-button v-auth="'sys:user:update'" type="primary" link @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
-					<el-button v-auth="'sys:user:delete'" type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
+					<el-button type="primary" link @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+					<el-button type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
 				</template>
 			</el-table-column>
 		</el-table>
+
 		<el-pagination
 			:current-page="state.page"
 			:page-sizes="state.pageSizes"
@@ -78,12 +65,11 @@ import cache from '@/utils/cache'
 import constant from '@/utils/constant'
 
 const state: IHooksOptions = reactive({
-	dataListUrl: '/sys/user/page',
+	dataListUrl: '/ass/check/page',
 	deleteUrl: '/sys/user',
 	queryForm: {
-		username: '',
-		mobile: '',
-		gender: ''
+		name: '',
+		reason: ''
 	}
 })
 
